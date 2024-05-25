55754811f13a622dc3e2846b88fed48281cf91cb
heartsantor <antorboss.bd@gmail.com>
2024-05-26 05:57:42 +0600

    fix: button

--- client/src/components/LunchItem.tsx
+++ client/src/components/LunchItem.tsx
@@ -9,6 +9,7 @@ import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import Delete from "@mui/icons-material/Delete";
 
@@ -110,7 +111,9 @@ const LunchItem: React.FC<LunchItemProps> = ({ item }) => {
           </>
         ) : (
           <>
-            <IconButton
+            <Button
+              variant="outlined"
+              size="small"
               aria-label="add to favorites"
               disabled={isEmployeeLoading}
               onClick={() =>
@@ -121,8 +124,8 @@ const LunchItem: React.FC<LunchItemProps> = ({ item }) => {
                 })
               }
             >
-              <FavoriteIcon />
-            </IconButton>
+              Choose
+            </Button>
             <div></div>
           </>
         )}
